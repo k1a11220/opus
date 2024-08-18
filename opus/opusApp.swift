d463939983967636bf382090fd1b3af1a4f5aa75
Beomsoo Son <sbs@henge.io>
2024-08-18 16:40:35 -0400

    Init

---
+++ opus/opusApp.swift
@@ -0,0 +1,17 @@
+//
+//  opusApp.swift
+//  opus
+//
+//  Created by Beomsoo on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct opusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
